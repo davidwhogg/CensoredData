3c9e08b0e8896dd817582def0db1d633579ca9fd
joeyrichar <joeyrichar@gmail.com>
2012-04-11 09:44:19 -0700

    minor bug fixes in censoring2.py.  it now runs for me in Python 2.7.2 | EPD 7.2-2

--- py/censoring2.py
+++ py/censoring2.py
@@ -6,6 +6,7 @@
 import numpy as np
 from scipy.stats import norm
 from scipy.stats import gamma
+from scipy import integrate
 
 def log_likelihood(t,f,e,tc,w,A0,A1,B1,su2,B,VB,Vsig,S,VS):
     """ 
@@ -24,7 +25,7 @@ def log_likelihood(t,f,e,tc,w,A0,A1,B1,su2,B,VB,Vsig,S,VS):
     ll = 0
     for i in uc:
         ll += np.log(likelihood_censored(i,su2,B,VB))
-    for i in np.range(u.size):
+    for i in np.arange(u.size):
         ll += np.log(likelihood_observed(u[i],f[i],e[i],su2,B,VB,Vsig,S,VS))
     return ll
 
@@ -34,17 +35,17 @@ def likelihood_censored(uc,su2,B,VB):
         return norm.cdf((bi - uc) / np.sqrt(sig2 + su2)) * (1 / (np.sqrt(2*np.pi*VB))) * np.exp(-(bi-B)**2/(2*VB))
     ## integrand of equation (12), integrate this across sig2
     def sig_integral(sig2,uc,su2,B,VB):
-        return integrate.quad(erf_integral,B-3*np.sqrt(VB),B+3*np.sqrt(VB), [sig2,uc,su2,B,VB]) * (1/VB)*gamma.pdf(sig2,B/VB)
-    return integrate.quad(sig_integral,0.0,10,[uc,su2,B,VB])
+        return integrate.quad(erf_integral,B-3*np.sqrt(VB),B+3*np.sqrt(VB), (sig2,uc,su2,B,VB))[0] * (1/VB)*gamma.pdf(sig2,B/VB)
+    return integrate.quad(sig_integral,0.0,10,(uc,su2,B,VB))[0]
     
-def liklihood_observed(u,f,e,su2,B,VB,Vsig,S,VS):
+def likelihood_observed(u,f,e,su2,B,VB,Vsig,S,VS):
     def integrand(sig2,u,f,e,su2,B,VB,Vsig,S,VS):
         p_not_cens = norm.cdf((f - B) / np.sqrt(VB))
         p_flux = norm.pdf((f - u) / np.sqrt(sig2 + su2))
         p_si = (1/Vsig)*gamma.pdf(e,sig2/Vsig)
         p_sig2 = (1/VB)*gamma.pdf(sig2,B/VB)
         return p_not_cens * p_flux * p_si * p_sig2
-    return integrate.quad(integrand,0.0,10,[u,f,e,su2,B,VB,Vsig,S,VS])
+    return integrate.quad(integrand,0.0,10,(u,f,e,su2,B,VB,Vsig,S,VS))[0]
 
 
 ### TODO: 1. intelligently choose limits for gamma integral
