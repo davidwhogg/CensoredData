5d4f766c7b040e933eb83ea95d56a69886855083
David W. Hogg <dwhogg@gmail.com>
2012-04-11 15:32:11 -0700

    small changes, bug fixes

--- py/fake_data.py
+++ py/fake_data.py
@@ -5,6 +5,7 @@ Copyright 2012 David W. Hogg (NYU).
 issues:
 -------
 - not tested
+- not properly commented
 '''
 
 if __name__ == '__main__':
@@ -23,27 +24,21 @@ def mu(times, omega, A0, A1, B1):
     Make the model mean flux given lightcurve parameters.
     '''
     return A0 \
-        + A1 * np.cos(2. * np.pi * omega * times) \
-        + A1 * np.sin(2. * np.pi * omega * times)
+        + A1 * np.cos(omega * times) \
+        + B1 * np.sin(omega * times)
 
-def make_fake_data(omega, A0, A1, B1, s2mu, B, VB, Vsigma, S, VS, size=1):
+def make_fake_data(times, omega, A0, A1, B1, s2mu, B, VB, Vsigma, S, VS):
     '''
     Implement the full generative model from the paper.  Is this
     correct?
     '''
-    times = 3. * np.random.uniform(size=size)
     mus = mu(times, omega, A0, A1, B1)
     sigma2s = Gamma.rvs(S**2/VS, scale=VS/S, size=times.size)
-    assert(np.all(sigma2s >= 0.))
     sobs2s = np.array([Gamma.rvs(m**2/Vsigma, scale=Vsigma/m, size=1)[0] for m in sigma2s])
-    assert(np.all(sobs2s >= 0.))
     fobss = mus + np.sqrt(sigma2s + s2mu) * Norm.rvs(size=times.size)
     bs = Gamma.rvs(B**2/VB, scale=VB/B, size=times.size)
-    assert(np.all(bs >= 0.))
     good = fobss > bs
     bad = good == False
-    print np.sum(good), times[good]
-    print np.sum(bad), times[bad]
     return times[good], fobss[good], sobs2s[good], times[bad]
 
 def plot_data(ax, goodtimes, fobss, sobs2s, badtimes):
@@ -59,12 +54,12 @@ def plot_model(ax, omega, A0, A1, B1):
     return None
 
 if __name__ == '__main__':
-    omega, A0, A1, B1 = 2.5, 5.0, 3.0, 0.5
+    omega, A0, A1, B1 = 2. * np.pi / 400., 5.0, 3.0, 0.5
     s2mu = 0.2**2
-    S, VS = 0.2, 0.04**2
-    B, VB = 3.5, 1.0**2
-    Vsigma = 0.1**2
-    gt, f, s2, bt = make_fake_data(omega, A0, A1, B1, s2mu, B, VB, Vsigma, S, VS, size=64)
+    S, VS = 0.7, 0.15**2
+    B, VB = 4.5, 0.4**2
+    Vsigma = 0.2**2
+    gt, f, s2, bt = make_fake_data(3600. * np.random.uniform(size=512), omega, A0, A1, B1, s2mu, B, VB, Vsigma, S, VS)
     ax = plt.subplot(111)
     plot_data(ax, gt, f, s2, bt)
     plot_model(ax, omega, A0, A1, B1)
