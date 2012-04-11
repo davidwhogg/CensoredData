3be174df8f8d18b9657a66be6a9e8d2af9684ed9
joeyrichar <joeyrichar@gmail.com>
2012-04-11 11:57:52 -0700

    changes to censoring2.py made during coffee hour on 04112012

--- py/censoring2.py
+++ py/censoring2.py
@@ -4,9 +4,15 @@
 ###
 
 import numpy as np
-from scipy.stats import norm
-from scipy.stats import gamma
+import scipy.stats as stats
+#from scipy.stats import norm as scipynorm
+#from scipy.stats import gamma as scipygamma
 from scipy import integrate
+
+
+
+def newgamma(mean,var):
+    return stats.gamma(mean**2/var, scale = var/mean)
 
 def log_likelihood(t,f,e,tc,w,A0,A1,B1,su2,B,VB,Vsig,S,VS):
     """ 
@@ -22,32 +28,29 @@ def log_likelihood(t,f,e,tc,w,A0,A1,B1,su2,B,VB,Vsig,S,VS):
     uc = A0 + A1*np.sin(tc*w*2*np.pi) + B1*np.cos(tc*w*2*np.pi)
 
     ## compute loglikelihood
-    ll = 0
-    for i in uc:
-        ll += np.log(likelihood_censored(i,su2,B,VB))
-    for i in np.arange(u.size):
-        ll += np.log(likelihood_observed(u[i],f[i],e[i],su2,B,VB,Vsig,S,VS))
-    return ll
-
-def likelihood_censored(uc,su2,B,VB):
+    return np.sum(loglikelihood_censored(uc,su2,B,VB,S,VS)) + \
+         np.sum(loglikelihood_observed(u,f,e,su2,B,VB,Vsig,S,VS))
+    
+def loglikelihood_censored(uc,su2,B,VB,S,VS):
     ## integrand of equation (7), integrate this across bi
     def erf_integral(bi,sig2,uc,su2,B,VB):
-        return norm.cdf((bi - uc) / np.sqrt(sig2 + su2)) * (1 / (np.sqrt(2*np.pi*VB))) * np.exp(-(bi-B)**2/(2*VB))
+        return stats.norm.cdf(uc, bi, np.sqrt(sig2 + su2)) \
+               * stats.gamma.pdf(bi,B**2/VB, scale = VB/B)
     ## integrand of equation (12), integrate this across sig2
-    def sig_integral(sig2,uc,su2,B,VB):
-        return integrate.quad(erf_integral,B-3*np.sqrt(VB),B+3*np.sqrt(VB), (sig2,uc,su2,B,VB))[0] * (1/VB)*gamma.pdf(sig2,B/VB)
-    return integrate.quad(sig_integral,0.0,10,(uc,su2,B,VB))[0]
-    
-def likelihood_observed(u,f,e,su2,B,VB,Vsig,S,VS):
-    def integrand(sig2,u,f,e,su2,B,VB,Vsig,S,VS):
-        p_not_cens = norm.cdf((f - B) / np.sqrt(VB))
-        p_flux = norm.pdf((f - u) / np.sqrt(sig2 + su2))
-        p_si = (1/Vsig)*gamma.pdf(e,sig2/Vsig)
-        p_sig2 = (1/VB)*gamma.pdf(sig2,B/VB)
-        return p_not_cens * p_flux * p_si * p_sig2
-    return integrate.quad(integrand,0.0,10,(u,f,e,su2,B,VB,Vsig,S,VS))[0]
+    def sig_integrand(sig2,uc,su2,B,VB,S,VS):
+        return integrate.quad(erf_integral,0.,B+5.*np.sqrt(VB), (sig2,uc,su2,B,VB))[0] \
+               * stats.gamma.pdf(sig2,S**2/VS, scale = VS/S)
+    return np.log([integrate.quad(sig_integrand,0.,S+5*np.sqrt(VS),(ui,su2,B,VB,S,VS))[0] for ui in uc])
 
+def loglikelihood_observed(u,f,e,su2,B,VB,Vsig,S,VS):
+    def integrand(sig2,u,f,e,su2,B,VB,Vsig,S,VS):
+        p_not_cens = stats.gamma.cdf(f,B**2/VB, scale = VB/B)
+        p_flux = stats.norm.pdf((f - u) / np.sqrt(sig2 + su2))
+        p_si = stats.gamma.pdf(e, sig2**2/Vsig, scale = Vsig/sig2)
+        p_sig2 = stats.gamma.pdf(sig2,S**2/VS, scale = VS/S)
+        return p_not_cens * p_flux * p_si * p_sig2
+    return np.log([integrate.quad(integrand,0.,S+5.*np.sqrt(VS),(ui,fi,ei,su2,B,VB,Vsig,S,VS))[0] for (ui,fi,ei) in zip(u,f,e)])
 
 ### TODO: 1. intelligently choose limits for gamma integral
 ###       2. check that my gamma interpretation is right
-
+###       3. audit all gamma and norm calls
