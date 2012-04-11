fa2da12775da7da4bcd64608390e839fdfe4704d
joeyrichar <joeyrichar@gmail.com>
2012-04-11 16:13:43 -0700

    fixed censoring2.py code

--- py/censoring2.py
+++ py/censoring2.py
@@ -1,6 +1,8 @@
 ###
 ### implements censoring model
 ### date April 10, 2012
+
+### Updated by JWR on 20120411
 ###
 
 import numpy as np
@@ -11,46 +13,57 @@ from scipy.stats import norm
 from scipy import integrate
 
 
+class Censored:
 
-def newgamma(mean,var):
-    return stats.gamma(mean**2/var, scale = var/mean)
-
-def log_likelihood(t,f,e,tc,w,A0,A1,B1,su2,B,VB,Vsig,S,VS):
-    """ 
-    computes log p(D|theta,I)
-    t: times when not censored (1-d ndarray)
-    f: flux when not censored (1-d ndarray)
-    e: reported error when not censored (1-d ndarray)
-    tc: times of censored observations (1-d ndarray)
-    everything else: same notation as paper (all floats)
-    """
+    def __init__(self, t, f, e, tc, tolquad = 0.1):
+        """ 
+        t: times when not censored (1-d ndarray)
+        f: flux when not censored (1-d ndarray)
+        e: reported error when not censored (1-d ndarray)
+        tc: times of censored observations (1-d ndarray)
+        """      
+        self.tolquad = tolquad # numerical tolerance in integration
+        # insert data 
+        self.t = t
+        self.f = f
+        self.e = e
+        self.tc = tc
+        
+    def log_likelihood(self,P,A0,A1,B1,su2,B,VB,Vsig,S,VS):
+        """ 
+        computes log p(D|theta,I)
+        everything else: same notation as paper (all floats)
+        """
     ## convert all times to expected flux (i.e. t_i -> u_i)
-    u = A0 + A1*np.sin(t*w*2*np.pi) + B1*np.cos(t*w*2*np.pi)
-    uc = A0 + A1*np.sin(tc*w*2*np.pi) + B1*np.cos(tc*w*2*np.pi)
+        w =  (2 * np.pi)/ P 
+        self.u = A0 + A1*np.cos(self.t*w) + B1*np.sin(self.t*w)
+        self.uc = A0 + A1*np.cos(self.tc*w) + B1*np.sin(self.tc*w)
 
     ## compute loglikelihood
-    return np.sum(loglikelihood_censored(uc,su2,B,VB,S,VS)) + \
-         np.sum(loglikelihood_observed(u,f,e,su2,B,VB,Vsig,S,VS))
+        return np.sum(self.loglikelihood_censored(su2,B,VB,S,VS)) + \
+               np.sum(self.loglikelihood_observed(su2,B,VB,Vsig,S,VS))
     
-def loglikelihood_censored(uc,su2,B,VB,S,VS):
+    def loglikelihood_censored(self,su2,B,VB,S,VS):
     ## integrand of equation (7), integrate this across bi
-    def erf_integral(bi,sig2,uc,su2,B,VB):
-        return stats.norm.cdf((uc - bi) / np.sqrt(sig2 + su2)) \
-               * stats.gamma.pdf(bi,B**2/VB, scale = VB/B)
+        def erf_integral(bi,ui,sig2,su2,B,VB):
+            return stats.norm.cdf((ui - bi) / np.sqrt(sig2 + su2)) \
+                   * stats.gamma.pdf(bi,B**2/VB, scale = VB/B)
     ## integrand of equation (12), integrate this across sig2
-    def sig_integrand(sig2,uc,su2,B,VB,S,VS):
-        return integrate.quad(erf_integral,0.,B+5.*np.sqrt(VB), (sig2,uc,su2,B,VB))[0] \
-               * stats.gamma.pdf(sig2,S**2/VS, scale = VS/S)
-    return np.log([integrate.quad(sig_integrand,0.,S+5*np.sqrt(VS),(ui,su2,B,VB,S,VS))[0] for ui in uc])
+        def sig_integrand(sig2,ui,su2,B,VB,S,VS):
+            return integrate.quad(erf_integral,0.,B+5.*np.sqrt(VB), (ui,sig2,su2,B,VB),epsabs = self.tolquad)[0] \
+                   * stats.gamma.pdf(sig2,S**2/VS, scale = VS/S)
+        return np.log([integrate.quad(sig_integrand,0.,S+5*np.sqrt(VS),(ui,su2,B,VB,S,VS),epsabs=self.tolquad)[0] \
+                       for ui in self.uc])
 
-def loglikelihood_observed(u,f,e,su2,B,VB,Vsig,S,VS):
-    def integrand(sig2,u,f,e,su2,B,VB,Vsig,S,VS):
-        p_not_cens = stats.gamma.cdf(f,B**2/VB, scale = VB/B)
-        p_flux = stats.norm.pdf((f - u) / np.sqrt(sig2 + su2))
-        p_si = stats.gamma.pdf(e, sig2**2/Vsig, scale = Vsig/sig2)
-        p_sig2 = stats.gamma.pdf(sig2,S**2/VS, scale = VS/S)
-        return p_not_cens * p_flux * p_si * p_sig2
-    return np.log([integrate.quad(integrand,0.,S+5.*np.sqrt(VS),(ui,fi,ei,su2,B,VB,Vsig,S,VS))[0] for (ui,fi,ei) in zip(u,f,e)])
+    def loglikelihood_observed(self,su2,B,VB,Vsig,S,VS):
+        def integrand(sig2,ui,fi,ei,su2,B,VB,Vsig,S,VS):
+            p_not_cens = stats.gamma.cdf(fi,B**2/VB, scale = VB/B)
+            p_flux = stats.norm.pdf((fi - ui) / np.sqrt(sig2 + su2))
+            p_si = stats.gamma.pdf(ei, sig2**2/Vsig, scale = Vsig/sig2)
+            p_sig2 = stats.gamma.pdf(sig2,S**2/VS, scale = VS/S)
+            return p_not_cens * p_flux * p_si * p_sig2
+        return np.log([integrate.quad(integrand,0.,S+5.*np.sqrt(VS),(ui,fi,ei,su2,B,VB,Vsig,S,VS),epsabs=self.tolquad)[0]\
+                       for (ui,fi,ei) in zip(self.u,self.f,self.e)])
 
 ### TODO: 1. intelligently choose limits for gamma integral
 ###       2. check that my gamma interpretation is right
