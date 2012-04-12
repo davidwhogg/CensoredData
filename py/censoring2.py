0daf68397a2ab7976232103a051f5b817a7da0ca
james <james@james-laptop.(none)>
2012-04-11 18:09:44 -0700

    changed scipy pdf calls to our own functions

--- py/censoring2.py
+++ py/censoring2.py
@@ -11,6 +11,7 @@ import scipy.stats as stats
 #from scipy.stats import gamma as scipygamma
 from scipy.stats import norm
 from scipy import integrate
+from scipy.special import gamma
 
 
 class Censored:
@@ -58,19 +59,26 @@ class Censored:
     ## integrand of equation (12), integrate this across sig2
         def sig_integrand(sig2,ui,su2,B,VB,S,VS):
             return  stats.norm.cdf((B-ui) / np.sqrt(VB + sig2 + su2)) * \
-                stats.gamma.pdf(sig2,S**2/VS, scale = VS/S)
+                gamma_pdf(sig2,S,VS)
         return np.log([integrate.quad(sig_integrand,0.,S+5*np.sqrt(VS),(ui,su2,B,VB,S,VS),epsabs=self.tolquad)[0] \
                        for ui in self.uc])
 
     def loglikelihood_observed(self,su2,B,VB,Vsig,S,VS):
         def integrand(sig2,ui,fi,ei,su2,B,VB,Vsig,S,VS):
             p_not_cens = stats.gamma.cdf(fi,B**2/VB, scale = VB/B)
-            p_flux = stats.norm.pdf((fi - ui) / np.sqrt(sig2 + su2))
-            p_si = stats.gamma.pdf(ei, sig2**2/Vsig, scale = Vsig/sig2)
-            p_sig2 = stats.gamma.pdf(sig2,S**2/VS, scale = VS/S)
+            p_flux = gaussian_pdf((fi - ui) / np.sqrt(sig2 + su2))
+            p_si = gamma_pdf(ei,sig2,Vsig)
+            p_sig2 = gamma_pdf(sig2,S,VS)
             return p_not_cens * p_flux * p_si * p_sig2
         return np.log([integrate.quad(integrand,0.,S+5.*np.sqrt(VS),(ui,fi,ei,su2,B,VB,Vsig,S,VS),epsabs=self.tolquad)[0]\
                        for (ui,fi,ei) in zip(self.u,self.f,self.e)])
+
+def gaussian_pdf(x):
+    return (1 / (np.sqrt(2.*np.pi))) * np.exp(-(x**2) / (2.))    
+
+def gamma_pdf(x,k,theta):
+    return (1 / ((theta**k)*gamma(k))) *  (x**(k-1.)) * np.exp(-x / theta)
+
 
 ### TODO: 1. intelligently choose limits for gamma integral
 ###       2. check that my gamma interpretation is right
