7e0151eb8587212d23c6192950dad4844a88ac1c
james <james@james-laptop.(none)>
2012-04-23 15:22:45 -0700

    lomb_scargle_censor_test.py tests nat's period finder code

---
+++ py/lomb_scargle_censor_test.py
@@ -0,0 +1,31 @@
+## code to test nat's function
+## by James Long on April 23, 2012
+
+import numpy as np
+from lomb_scargle_censor import lomb
+
+# get data in nat's preferred form
+i = "../data/lc_085647-6242.9.dat"
+lc = np.fromfile(i, dtype=float, count=-1, sep=" ")
+lc = lc.reshape((lc.size / 3,3))
+time_missing = lc[lc[:,1] > 29.9,0]
+lc = lc[lc[:,1] < 29.99,:]
+
+# l.s. grid search parameters
+min_freq = .002
+max_freq = 1.
+df = .000001
+numf = int((max_freq - min_freq) / df)
+
+## best period using censored data
+results = lomb(lc[:,0],lc[:,1],lc[:,2],time_missing,min_freq,df,numf)
+best = np.argmax(results)
+period = 1 / (min_freq + best * df)
+print period
+
+## best period ignoring censoring
+none_missing = np.array([],dtype='float64')
+results = lomb(lc[:,0],lc[:,1],lc[:,2],none_missing,min_freq,df,numf)
+best = np.argmax(results)
+period = 1 / (min_freq + best * df)
+print period
